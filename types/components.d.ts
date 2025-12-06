01ad3902ccd9626b241ea41afada0edb574ffdf7
Francisco Gutiérrez <pikilon@gmail.com>
2025-12-06 16:34:42 +0100

    removes any old file from original project

--- types/components.d.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Properties for the DieDice component
- */
-export interface DieDiceProps {
-  /** The sides of the die */
-  sides: string[];
-  /** The currently displayed side */
-  _current: string;
-}
--- types/jsurl.d.ts
+++
@@ -1,5 +0,0 @@
-declare module "jsurl" {
-  export function parse(input: string): any;
-  export function stringify(input: any): string;
-  export function stringify(input: any): string;
-}
