d703298ba9c604cba73f616bf3c518223eb34ee3
Francisco Gutiérrez <pikilon@gmail.com>
2025-12-21 14:18:36 +0100

    refactor: remove DiceDictionaryListComponent and clean up related code

--- js/main.js
+++ js/main.js
@@ -12,24 +12,23 @@ import "./modules/DiceThrowerComponent.js";
 import "./modules/DiceResultComponent.js";
 import "./modules/DiceInstructionComponent.js";
 import "./modules/DicePreviewComponent.js";
-import "./modules/DiceDictionaryListComponent.js";
 import "./modules/DiceGameCardComponent.js";
 import "./modules/DiceGameSetDrawerComponent.js";
 
 function dice_initialize() {
   document.getElementById("loading_text")?.remove();
-  
+
   // Configurar uso de random
   setUseTrueRandom(false);
-  
+
   // Los web components se encargan de todo
   // Suscribirse a cambios para logging (opcional)
-  gameState.subscribe('all', (state) => {
-    console.log('Game state updated:', state);
+  gameState.subscribe("all", (state) => {
+    console.log("Game state updated:", state);
   });
-  
+
   // Aplicar clase noselect al body por defecto
-  document.body.classList.add('noselect');
+  document.body.classList.add("noselect");
 }
 
 dice_initialize();
